5576c81023cca33f7a1d3e987e8be5dfad90832e
Radek Čep <31346847+radekcep@users.noreply.github.com>
2022-01-12 12:01:16 +0100

    Added Xcode project

---
+++ ScreenCast/ScreenCastApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenCastApp.swift
+//  ScreenCast
+//
+//  Created by Radek Čep on 12.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct ScreenCastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
