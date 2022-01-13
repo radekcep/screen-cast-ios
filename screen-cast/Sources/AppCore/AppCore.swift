62fcdd5c5f4011888dfdf76eb25e8289b7feea1c
Radek Čep <31346847+radekcep@users.noreply.github.com>
2022-01-13 17:32:36 +0100

    Changed project structure

---
+++ screen-cast/Sources/AppCore/AppCore.swift
@@ -0,0 +1,12 @@
+//
+//  AppCore.swift
+//  
+//
+//  Created by Radek Čep on 13.01.2022.
+//
+
+import Foundation
+
+public class AppCore {
+    
+}
---
+++ screen-cast/Tests/AppCoreTests/AppCoreTests.swift
@@ -0,0 +1,15 @@
+//
+//  AppCoreTests.swift
+//  
+//
+//  Created by Radek Čep on 13.01.2022.
+//
+
+import XCTest
+
+@testable import AppCore
+
+class AppCoreTests: XCTestCase {
+    func testExample() throws {
+    }
+}
